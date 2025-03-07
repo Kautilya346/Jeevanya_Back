2e9d4d5238d4dc5a3e6581618b45321430eecef2
Kautilya03 <23ucs613@gmail.com>
2025-03-07 23:24:55 +0530

    init done

---
+++ index.js
@@ -0,0 +1,40 @@
+import express from "express";
+import bodyParser from "body-parser";
+import connectDB from "./Utils/db.js";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+const app = express();
+dotenv.config();
+connectDB();
+const PORT = 3000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+const allowedOrigins = ["http://localhost:5173"];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, origin);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"], 
+  })
+);
+app.options("*", cors());
+
+// Middleware
+app.use(bodyParser.json());
+// Server
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
